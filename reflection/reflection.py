ab7ec9e0a8da7ab2326a605839cb87be79b1dd69
Eric W. Wallace <e@ewall.org>
2017-06-29 19:57:07 -0400

    studio_view: add checkboxes for picking prompts, plus layout details

--- reflection/reflection.py
+++ reflection/reflection.py
@@ -34,8 +34,8 @@ class ReflectionAssistantXBlock(XBlock):
         """
         html = self.resource_string("static/html/reflection.html")
         frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/reflection.css"))
         frag.add_css(self.resource_string("static/css/edx_pattern_library.css"))
+        frag.add_css(self.resource_string("static/css/reflection.css"))
         frag.add_javascript(self.resource_string("static/js/src/reflection.js"))
         frag.initialize_js('ReflectionAssistantXBlock')
         return frag
@@ -48,8 +48,8 @@ class ReflectionAssistantXBlock(XBlock):
         """
         html = self.resource_string("static/html/reflection_edit.html")
         frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/reflection_edit.css"))
         frag.add_css(self.resource_string("static/css/edx_pattern_library.css"))
+        frag.add_css(self.resource_string("static/css/reflection_edit.css"))
         frag.add_javascript(self.resource_string("static/js/src/reflection_edit.js"))
         frag.initialize_js('ReflectionAssistantXBlock')
         return frag
