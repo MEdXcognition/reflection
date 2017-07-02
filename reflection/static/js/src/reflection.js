ffca46c22f012e766092d2d02079b61bc9e7c955
Eric W. Wallace <e@ewall.org>
2017-07-02 16:04:48 -0400

    student_view: add form validation

--- reflection/static/js/src/reflection.js
+++ reflection/static/js/src/reflection.js
@@ -3,6 +3,7 @@ function ReflectionAssistantXBlock(runtime, element, config) {
 
     /* Page Load Actions */
     $(function ($) {
+        /* Display chosen block type */
         switch (config.block_type) {
             case "pre":
                 $("#prompt-pre").show();
@@ -10,7 +11,21 @@ function ReflectionAssistantXBlock(runtime, element, config) {
             case "post":
                 $("#prompt-post").show();
                 break;
-        }
+        };
+
+        /* Form Validation */
+        var parsley_options = {
+            excluded: 'input:disabled,input:hidden',
+            trigger: 'keyup',
+            errorClass: 'has-error',
+            successClass: 'has-success',
+            errorsWrapper: '<div class="field-message has-error"></div>',
+            errorTemplate: '<span class="field-message-content"></span>'
+        };
+        $('#form-prompt-pre,#form-prompt-post').parsley(parsley_options).on('form:submit', function() {
+            return false;
+        });
+
     });
 
 }
